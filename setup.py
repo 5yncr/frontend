1fb7844412b21888fca32cab8a37910116f08a8b
Alex Tryjankowski <aet41@case.edu>
2018-02-23 15:19:03 -0500

    Removed login references and updated naming conventions

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='frontend',
-    packages=['frontend'],
+    name='5yncr Frontend',
+    version='0.0.1',
+    packages=['syncr_frontend'],
     include_package_data=True,
     install_requires=[
         'flask',
