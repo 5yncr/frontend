a1e891f39b101231e6b0f26e8db4c86223e93ef9
AlexTryjan <aet41@case.edu>
2018-02-22 20:10:57 -0500

    Created simple drop view section & Added subscriber buttons/connected them to Flask templates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # frontend
+# Setup Virtual Environment: virtualenv venv
+# Run Virtual Environment: Mac - venv source/bin/activate Windows - venv\Scripts\activate
+# Install Requirements: pip install -r requirements.txt
+# Set Flask Variables: Mac - export FLASK_APP=frontend Windows - set FLASK_APP=frontend
+# Run Flask App: flask run OR python -m flask run
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -15,32 +15,59 @@ app.config.from_envvar('SYNCR_SETTINGS', silent=True)
 
 
 def get_owned_drops():
+    # Placeholder until backend communication is set-up
+    # TODO: validate data structure
     return [{'name': 'O_Drop_1'}, {'name': 'O_Drop_2'}]
 
 
 def get_subscribed_drops():
+    # Placeholder until backend communication is set-up
+    # TODO: validate data structure
     return [{'name': 'S_Drop_1'}, {'name': 'S_Drop_2'}, {'name': 'S_Drop_3'}]
 
 
 def get_selected_drop(drop_id):
-    return [drop_id]
+    # if drop does not exist -> return default drop display
+    return {'name': drop_id}
+
+
+@app.route('/unsubscribe/<drop_id>')
+def unsubscribe(drop_id):
+    # if no drop is selected
+        # Do nothing
+    # else
+        # communicate change to backend
+    return show_drops(None, "unsubscribed")
+
+
+# Request a change to the selected drop
+@app.route('/request_change/<drop_id>')
+def request_change(drop_id):
+    # if no drop is seleted
+        # Do nothing
+    # else
+        # communicate change to backend
+    return show_drops(drop_id, "change requested")
 
 
 @app.route('/')
 def startup():
-    return show_drops(None)
+    return show_drops(None, None)
 
 
 @app.route('/<drop_id>')
-def show_drops(drop_id=None):
+def show_drops(drop_id=None, message=None):
     owned_drops = get_owned_drops()
     subscribed_drops = get_subscribed_drops()
     selected_drop = []
     if drop_id is not None:
         selected_drop = get_selected_drop(drop_id)
+    performed_action = []  # REMOVE WHEN BACKEND COMMUNICATION IS ADDED
+    if message is not None:
+        performed_action = {'description': message}
     return render_template(
-        'show_drops.html', selected=selected_drop,
-        subscribed=subscribed_drops, owned=owned_drops,
+        'show_drops.html', selected=selected_drop, subscribed=subscribed_drops,
+        owned=owned_drops, action=performed_action,
     )
 
 
