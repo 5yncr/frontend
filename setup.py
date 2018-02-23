a5613c0b571e22bbac1bb40fbba4f1f1f39feb46
Alexander Tryjankowski <aet41@case.edu>
2018-02-23 16:06:09 -0500

    Add License Info

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='5yncr Frontend',
     version='0.0.1',
     packages=['syncr_frontend'],
+    license='AGPLv3',
     include_package_data=True,
     install_requires=[
         'flask',
