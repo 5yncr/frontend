d1f52716363d0459ac0e3fe84b29a32162db79e2
Jack LaRue <jvl13@case.edu>
2018-04-13 16:21:44 -0400

    Add functionality to select directory to initialize drop
    Signed-off-by: Jack LaRue <jvl13@case.edu>

--- syncr_frontend/frontend.py
+++ syncr_frontend/frontend.py
@@ -9,7 +9,7 @@ from flask import request
 from tkinter import filedialog
 from tkinter import Tk
 
-# from . import communication
+from . import communication
 
 app = Flask(__name__)  # create the application instance
 app.config.from_object(__name__)  # load config from this file , frontend.py
@@ -23,6 +23,7 @@ app.config.from_envvar('SYNCR_SETTINGS', silent=True)
 
 # Global Variables
 curr_action = ''
+current_drop_path = ''
 change_list = []
 
 # Backend Access Functions
@@ -36,46 +37,7 @@ def send_message(message):
     :return: response from server
     """
 
-    # response = communication.send_message(message)
-
-    response = {
-        'change_list': message.get('change_list'),
-        'drop_id': message.get('drop_id'),
-        'drop_name': message.get('drop_name'),
-        'file_name': message.get('file_name'),
-        'file_path': message.get('file_path'),
-        'action': message.get('action'),
-        'message': "Generic Message For " + message.get('action'),
-        'success': True,
-        'requested_drops': (
-            {
-                'drop_id': 'o1',
-                'name': 'O_Drop_1',
-                'version': None,
-                'previous_versions': [],
-                'primary_owner': 'p_owner_id',
-                'other_owners': ["owner1", "owner2"],
-                'signed_by': 'owner_id',
-                'files': [
-                    {'name': 'FileOne'},
-                    {'name': 'FileTwo'},
-                    {'name': 'FileThree'},
-                    {'name': 'FileFour'},
-                    {'name': 'Folder'},
-                ],
-            },
-            {
-                'drop_id': 'o2',
-                'name': 'O_Drop_2',
-                'version': None,
-                'previous_versions': [],
-                'primary_owner': 'owner_id',
-                'other_owners': [],
-                'signed_by': 'owner_id',
-                'files': [],
-            },
-        ),
-    }
+    response = communication.send_message(message)
 
     return response
 
@@ -329,27 +291,60 @@ def transfer_ownership():
     )
 
 
-@app.route('/input_name', methods=['POST'])
-def input_name():
+@app.route('/select_directory')
+def select_directory():
+    """
+    Allows user to select directory to turn into drop.
+    :return: directory path is stored.
+    """
+    global current_drop_path
+
+    root = Tk()
+    root.filename = filedialog.askopenfilename(
+        initialdir="/", title="Select file",
+        filetypes=[("all files", "*.*")],
+    )
+
+    directory_path = root.filename
+    root.destroy()
+
+    if directory_path is None:
+        flash("No directory selected")
+    else:
+        current_drop_path = directory_path
+
+    return show_drops(None, None)
+
+
+@app.route('/initialize_drop')
+def initialize_drop():
     """
     After inputting a name, a drop is created with said name.
     :return: Message sent back to frontend.
     """
-    result = request.form.get('inputted_drop_name')
+    global current_drop_path
+    response = ''
 
-    # TODO: Get directory path from user in frontend.
-    # TODO: Replace name input with selecting
+    if current_drop_path == '':
+        flash('Cannot create drop. No directory was selected.')
+        has_response = False
+    else:
+        message = {
+            'action': 'initialize_drop',
+            'directory': current_drop_path,
+        }
+        has_response = True
+        response = send_message(message)
+        current_drop_path = ''
 
-    message = {
-        'action': 'input_name',
-        'directory': result,
-    }
-
-    response = send_message(message)
+    if has_response:
+        message = response.get('message')
+    else:
+        message = None
 
     return show_drops(
         None,
-        response.get('message'),
+        message,
     )
 
 
